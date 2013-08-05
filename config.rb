5835e7256f1763a54eb6e76d0324bed99432bbba
ChaYoung <yousbe@gmail.com>
2013-08-05 18:24:47 +0900

    Change root dir to /blog

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/blog/"
+http_images_path = "/blog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/blog/fonts"
+css_dir = "public/blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
