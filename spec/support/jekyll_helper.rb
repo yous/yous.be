0f7c413213d158c8bcf1729ea9935a5c02b2a16b
Chayoung You <yousbe@gmail.com>
2015-09-05 21:25:34 +0900

    Build site on `rake spec`

--- spec/support/jekyll_helper.rb
+++ spec/support/jekyll_helper.rb
@@ -3,7 +3,6 @@ require 'jekyll'
 module JekyllHelper
   def site
     unless @site
-      Jekyll::Commands::Clean.process({})
       @site = Jekyll::Site.new(
         Jekyll.configuration('serving' => false, 'full_rebuild' => true))
       @site.process
